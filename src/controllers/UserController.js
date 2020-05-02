522e8795e978524728f840a505aba71259b5a144
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-02 20:24:18 -0300

    Insersão de dados com knex

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,7 +5,20 @@ module.exports = {
         const results = await knex('users');
         
         return res.json(results);
+    },
+
+    async create(req, res, next){
         
-    
+        try{
+            const { username } = req.body;
+            await knex ('users').insert({ username });
+            
+            return res.status(201).send();
+        } catch (error){
+            next(error);
+
+        }
+        
     }
+
 }
--- src/server.js
+++ src/server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const routes = require('./routes')
 
 const app = express();
+app.use(express.json());
 app.use(routes);
 
+/* catch all (capturar todos os erros)
+   links de HTTP Status de erros
+   https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status 
+*/
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({ error: error.message });
+})
 
 app.listen(3333, ()=> console.log('Servidor ON - Rodando na porta 3333'));
