c3633ba65c3b18b87cf09a0ac4caa49b135c05ad
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-02 19:47:15 -0300

    Listando dados com Knex / Controller / Routes

--- src/server.js
+++ src/server.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const knex = require('./database/index.js');
+const routes = require('./routes')
 
 const app = express();
-
-
-app.get('/users', (req, res)=>{
-    knex('users').then((results)=>{
-        return res.json(results);
-    })
-
-});
+app.use(routes);
 
 
 app.listen(3333, ()=> console.log('Servidor ON - Rodando na porta 3333'));
