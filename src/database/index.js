557c1a0980527e75005887ddf287a6a131abf6a6
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-02 16:48:53 -0300

    Rota de listagem de usuários

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,4 @@
 const knexfile = require('../../knexfile');
-const knex = require('kenx')(knexfile['develoment']);
+const knex = require('knex')(knexfile['development']); 
 
 module.exports = knex;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const knex = require('./database/index.js');
+
 const app = express();
+
+
+app.get('/users', (req, res)=>{
+    knex('users').then((results)=>{
+        return res.json(results);
+    })
+
+});
+
 
 app.listen(3333, ()=> console.log('Servidor ON - Rodando na porta 3333'));
