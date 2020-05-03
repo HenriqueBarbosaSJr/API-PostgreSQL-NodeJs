3efc302971807e8947dfebbd24e5100a73d0b9d7
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-02 21:07:33 -0300

    Tratamento de error para digitação de end inexistente

--- src/server.js
+++ src/server.js
@@ -5,11 +5,20 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+/* Tratamento de error caso o cliente digite uma
+   rota deconhecida pelo servidor
+*/
+app.use((req, res, next) =>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+
 /* catch all (capturar todos os erros)
    links de HTTP Status de erros
    https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status 
 */
-
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({ error: error.message });
